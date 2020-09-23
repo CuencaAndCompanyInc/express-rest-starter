5a307af58a9c2d9dc397b61806591da5db54dc11
Abraham Cuenca <websiteprojectmanager@gmail.com>
2020-09-23 16:40:56 -0700

    Update test variables

--- src/server.test.js
+++ src/server.test.js
@@ -26,7 +26,7 @@ describe('Server.js', () => {
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
     };
-   return await request(app).post('/api/v1/user').send(mockUser);
+    return await request(app).post('/api/v1/user').send(mockUser);
   });
 
   afterEach(function () {
@@ -35,39 +35,40 @@ describe('Server.js', () => {
 
   describe('/api/v1/health', () => {
     it('should return health status of 200', async () => {
-      const res = await request(app).get('/api/v1/health');
-      expect(res.statusCode).toEqual(200);
+      const result = await request(app).get('/api/v1/health');
+      expect(result.statusCode).toEqual(200);
     });
   });
 
   describe('/api/v1/protected', () => {
-    const protectedUrl = '/api/v1/protected'
-    it('should return 401 ', async () => {
+    const protectedUrl = '/api/v1/protected';
+    it('should return 401 when no authToken is provided.', async () => {
       const result = await request(app).get(protectedUrl);
       expect(result.statusCode).toEqual(401);
     });
 
     it('should return protected data when authToken is provided', async () => {
-        const { username, firstName, lastName } = mockUser;
+      const { username, firstName, lastName } = mockUser;
 
       const token = jwt.sign(
-          {
-            user: {
-              username,
-              firstName,
-              lastName,
-            },
+        {
+          user: {
+            username,
+            firstName,
+            lastName,
           },
-          JWT_SECRET,
-          {
-            algorithm: 'HS256',
-            subject: username,
-            expiresIn: '7d',
-          },
+        },
+        JWT_SECRET,
+        {
+          algorithm: 'HS256',
+          subject: username,
+          expiresIn: '7d',
+        },
       );
 
-      const result = await request(app).get(protectedUrl)
-          .set('Authorization', `Bearer ${token}`);
+      const result = await request(app)
+        .get(protectedUrl)
+        .set('Authorization', `Bearer ${token}`);
       expect(result.statusCode).toBe(200);
       expect(result.body.data).toBe('Hello, World!');
     });
