f0796cebdce66d9794bf93c2b8003c96d47c9eba
Abraham Cuenca <websiteprojectmanager@gmail.com>
2020-09-22 22:14:36 -0700

    Add default value for JWT_SECRET

--- src/core/config/app/index.js
+++ src/core/config/app/index.js
@@ -9,5 +9,5 @@ exports.TEST_DATABASE_URL =
   global.TEST_DATABASE_URL ||
   'mongodb://localhost/express-rest-starter-test';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 's3cr3t!';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
